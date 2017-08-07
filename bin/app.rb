74c8556e6239966c9f5b0eecdbc13eaea61b8a1d
ivan <ivan7en@gmail.com>
2017-08-07 16:13:34 +0300

    change channel to main

--- bin/app.rb
+++ bin/app.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require_relative '../bot'
-telegram = LozovaNewsBot.new('@tst_lz')
 while true
+  telegram = LozovaNewsBot.new('@lozova_news')
   telegram.sync
   telegram.send
   sleep 300
