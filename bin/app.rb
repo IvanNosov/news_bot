b602cada09f8572f7465a0f97c5ce5e92c0bce7e
Ivan Nosov <ivan7en@gmail.com>
2018-02-19 18:03:18 +0200

    switched to test chanel

--- bin/app.rb
+++ bin/app.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require_relative '../bot'
 loop do
-  telegram = LozovaNewsBot.new('@lozova_news')
+  telegram = LozovaNewsBot.new('@testlozova')
   telegram.sync
   telegram.send
   sleep 300
